57772e1a4ef587c8951c5f359ae8e2af6b068db3
Mark Foskett <markryanfoskett@gmail.com>
2023-07-05 23:34:59 +1000

    added gradient background

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { GradientBackgroundCon } from '@/components/quote_generator/QuoteGeneratorElements'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Quote Generator</title>
+        <meta name="description" content="Generates an inspirational quote!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GradientBackgroundCon>
+
+      </GradientBackgroundCon>
+    </>
+  )
+}

8f0ad00c0e3ae70113e619bb77585d62f9cf2d4b
Mark Foskett <markryanfoskett@gmail.com>
2023-07-06 23:12:25 +1000

    edited title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Quote Generator</title>
+        <title>Inspirational Quote Generator</title>
         <meta name="description" content="Generates an inspirational quote!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />

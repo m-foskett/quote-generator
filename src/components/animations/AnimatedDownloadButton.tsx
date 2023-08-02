81787cc42aa8b7379a515eabc471e5036c9a4ecb
Mark Foskett <markryanfoskett@gmail.com>
2023-08-02 21:07:46 +1000

    final touchups

--- src/components/animations/AnimatedDownloadButton.tsx
+++ src/components/animations/AnimatedDownloadButton.tsx
@@ -1,6 +1,6 @@
+// Standard Imports
 import React from 'react'
-import Image from 'next/image'
-// React Lottie Image
+// React Lottie Image Imports
 import Lottie from 'react-lottie-player'
 import lottieJson from '../../assets/data-center-animation.json'
 
@@ -14,7 +14,7 @@ const AnimatedDownloadButton = ({handleDownload}: AnimatedDownloadButtonProps) =
     <div
       className='border-2 border-solid border-primary-950 relative rounded-3xl mt-5 scale-[0.7] m-auto
       hover:brightness-150 hover:ease-in-out hover:scale-[0.8] hover:origin-center neon-primary backdrop-filter
-      backdrop-blur-sm cursor-pointer w-[600px] h-[250px]'
+      backdrop-blur-sm cursor-pointer w-[400px] h-[250px]'
       onClick={handleDownload}
     >
       {/* Lottie Image */}
@@ -27,7 +27,7 @@ const AnimatedDownloadButton = ({handleDownload}: AnimatedDownloadButtonProps) =
       {/* Download Quote Card Container Text */}
       <div
         className='text-primary-50 font-serif text-2xl relative w-full text-center py-0 px-5 -mt-[30px] mb-[10px]
-        pointer-events-none sm:text-4xl'
+        pointer-events-none sm:text-3xl'
       >
         Download your quote card!
       </div>
